6c8f3a028142adc344ba387ea0dc6d3d8004ab68
thanaphon0737 <35342731+thanaphon0737@users.noreply.github.com>
2019-10-29 00:27:45 +0700

    layout customerdetail

--- resources/views/customerdetail.blade.php
+++ resources/views/customerdetail.blade.php
@@ -11,8 +11,8 @@ session_start();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Language" content="en">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
     <title>Products</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
     <meta name="description" content="This is an example dashboard created using build-in elements and components.">
@@ -27,8 +27,9 @@ session_start();
     =========================================================
     * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
     -->
-    
-<link href="../main.css" rel="stylesheet"></head>
+
+    <link href="../main.css" rel="stylesheet">
+</head>
 
 <body>
 
@@ -63,29 +64,29 @@ session_start();
                         </span>
                     </button>
                 </span>
-            </div>    
-            <div class="app-header__content" >
+            </div>
+            <div class="app-header__content">
                 <div class="app-header-right">
                     <div class="header-btn-lg pr-0">
                         <div class="widget-content p-0">
                             <div class="widget-content-wrapper">
-                            <!-- <div class="widget-heading">&nbsp</div> -->
+                                <!-- <div class="widget-heading">&nbsp</div> -->
                                 <div class="widget-content-left  header-user-info">
                                     <div class="widget-heading">
-                                    <?php echo session('name'); ?>
+                                        <?php echo session('name'); ?>
                                     </div>
                                     <div class="widget-subheading">
                                         <?php
-                                        if(session('status')=="1"){
+                                        if (session('status') == "1") {
                                             echo "Employee";
-                                        }else{
+                                        } else {
                                             echo "Admin";
                                         }
                                         ?>
                                     </div>
                                 </div>
                                 <div class="widget-content-left ml-3" id="sidebar4">
-                                
+
                                     <div class="btn-group">
                                         <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="p-0 btn">
                                             <img width="42" class="rounded-circle" src="../assets/images/avatars/1.jpg" alt="">
@@ -93,138 +94,180 @@ session_start();
                                         </a>
                                         <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right">
                                             <button type="button" tabindex="0" class="dropdown-item"><?php echo session('email'); ?></button>
-                                            <form action="/logout" method="post"> 
+                                            <form action="/logout" method="post">
                                                 {{ csrf_field() }}
                                                 <button tabindex="0" class="dropdown-item pe-7s-unlock">&nbsp Logout</button>
                                                 </button>
                                             </form>
-                                            
+
                                         </div>
                                     </div>
                                 </div>
-                               
-                               
-                            </div>
-                        </div>
-                    </div>        
-                </div>
-            </div>
-        </div>        
-        <div class="app-main">
-                <div class="app-sidebar sidebar-shadow bg-vicious-stance sidebar-text-light" id="sidebar">
-                    <div class="app-header__logo">
-                        <div class="logo-src"></div>
-                        <div class="header__pane ml-auto">
-                            <div>
-                                <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
-                                    <span class="hamburger-box">
-                                        <span class="hamburger-inner"></span>
-                                    </span>
-                                </button>
+
+
                             </div>
                         </div>
                     </div>
-                    <div class="app-header__mobile-menu">
+                </div>
+            </div>
+        </div>
+        <div class="app-main">
+            <div class="app-sidebar sidebar-shadow bg-vicious-stance sidebar-text-light" id="sidebar">
+                <div class="app-header__logo">
+                    <div class="logo-src"></div>
+                    <div class="header__pane ml-auto">
                         <div>
-                            <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                            <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
                                 <span class="hamburger-box">
                                     <span class="hamburger-inner"></span>
                                 </span>
                             </button>
                         </div>
                     </div>
-                    <div class="app-header__menu">
-                        <span>
-                            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
-                                <span class="btn-icon-wrapper">
-                                    <i class="fa fa-ellipsis-v fa-w-6"></i>
-                                </span>
-                            </button>
-                        </span>
-                    </div>    <div class="scrollbar-sidebar">
-                        <div class="app-sidebar__inner">
-                            <ul class="vertical-nav-menu">
-                                
+                </div>
+                <div class="app-header__mobile-menu">
+                    <div>
+                        <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                            <span class="hamburger-box">
+                                <span class="hamburger-inner"></span>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div class="app-header__menu">
+                    <span>
+                        <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                            <span class="btn-icon-wrapper">
+                                <i class="fa fa-ellipsis-v fa-w-6"></i>
+                            </span>
+                        </button>
+                    </span>
+                </div>
+                <div class="scrollbar-sidebar">
+                    <div class="app-sidebar__inner">
+                        <ul class="vertical-nav-menu">
+
                             <li class="app-sidebar__heading">Dashboard</li>
                             <li>
-                                    <a href="dashboard-boxes.html">
-                                        <i class="metismenu-icon pe-7s-display2"></i>
-                                        Dashboard
-                                    </a>
-                                </li>
+                                <a href="../dashboard-boxes.html">
+                                    <i class="metismenu-icon pe-7s-display2"></i>
+                                    Dashboard
+                                </a>
+                            </li>
 
 
-                                <li class="app-sidebar__heading">Menu</li>
-                                <li>
-                                    <a href="products">
-                                        <i class="metismenu-icon pe-7s-box2"></i>
-                                        Products
-                                    </a>
-                                </li>
-                               
-                                <li>
-                                    <a href="dashboard-boxes.html">
-                                        <i class="metismenu-icon pe-7s-box1"></i>
-                                        Stock In
-                                    </a>
-                                </li>
-                                
-                                <li>
-                                    <a href="customer" class="mm-active">
-                                        <i class="metismenu-icon pe-7s-users"></i>
-                                        Customers
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="dashboard-boxes.html">
-                                        <i class="metismenu-icon pe-7s-note2"></i>
-                                        Saleorder
-                                    </a>
-                                </li>
-                                
-                               
-                            </ul>
-                        </div>
+                            <li class="app-sidebar__heading">Menu</li>
+                            <li>
+                                <a href="../products">
+                                    <i class="metismenu-icon pe-7s-box2"></i>
+                                    Products
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../dashboard-boxes.html">
+                                    <i class="metismenu-icon pe-7s-box1"></i>
+                                    Stock In
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../customer" class="mm-active">
+                                    <i class="metismenu-icon pe-7s-users"></i>
+                                    Customers
+                                </a>
+                            </li>
+                            <li>
+                                <a href="../dashboard-boxes.html">
+                                    <i class="metismenu-icon pe-7s-note2"></i>
+                                    Saleorder
+                                </a>
+                            </li>
+
+
+                        </ul>
                     </div>
-                </div>    <div class="app-main__outer">
-                
-  
-
-              
-               
-    <div id="id02" class="modal" id="detailpopup"> 
-        <form class="modal-content-detail"> 
-            <div id="detailpop">
+                </div>
             </div>
-        </form> 
-    </div>
+            <div class="app-main__outer">
 
- 
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="main-card mb-3 card">
-                                    <div class="card-header">
-                                    <div class="col-md-3">
-                                        Customer Detail
-                                    </div>  
-                                  
-                                </div>
-                                    <?php echo $jscustomerdetail ?>
-                                </div>
+
+
+
+
+                <div id="id02" class="modal" id="detailpopup">
+                    <form class="modal-content-detail">
+                        <div id="detailpop">
+                        </div>
+                    </form>
+                </div>
+
+
+                <div class="row">
+                    <!-- start -->
+                    <div class="col-md-12">
+                        <div class="main-card mb-3 card">
+                            <div class="card-header mr-3">Customer Detail
+
                             </div>
+                            <div class="row">
+                                <div class="col-md-6 ml-auto">
+                                    <div class="main-card mb-3 card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-md-auto">
+                                                    <img class="d-block w-20" src="https://via.placeholder.com/200x150">
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div id="cusdetail"></div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="main-card mb-3 card">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Address</h5>
+                                            <ul class="list-group">
+                                                <li class="list-group-item">
+                                                    <div id="addr"></div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="main-card mb-3 card">
+                                        <div class="card-body">
+                                            <b class="card-title">Order</b>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+
                         </div>
                     </div>
-                    
+
+
                     <script src="../assets/scripts/htmlGen.js" type="text/javascript"></script>
                     <script src="../assets/scripts/jquery-3.4.1.js" type="text/javascript"></script>
                     <script type="text/javascript">
-                                 
+
                     </script>
-                    
-                                               
-                                            
-                <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
-        </div>
-    </div>
-<script type="text/javascript" src="../assets/scripts/main.js"></script></body>
+
+
+
+                    <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+                </div>
+            </div>
+            <script type="text/javascript" src="../assets/scripts/main.js"></script>
+</body>
+
 </html>
